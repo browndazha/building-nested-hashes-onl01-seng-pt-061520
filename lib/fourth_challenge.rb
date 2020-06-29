6abee0dfbc1913d9aeba88bf14544b95c6571748
Dazha Brown <dajahbrown@hotmail.com>
2020-06-29 05:47:06 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,8 +5,7 @@
         {:name => "Benvolio", :age => "17", :attitude => "worried"},
         {:name => "Steven", :age => "30", :attitude => "confused"}
       ]
-   },
-   },
+
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
