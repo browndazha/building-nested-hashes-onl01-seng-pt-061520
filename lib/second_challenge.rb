b0681beb5ed2affaa8db51de178177d34852080a
Dazha Brown <dajahbrown@hotmail.com>
2020-06-29 05:41:24 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,19 +1,24 @@
 def second_challenge
   epic_tragedy = {
-   :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-   },
-   :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
-   }
+    :montague => {
+      #your key/value pairs here!
+      },
+        :patriarch => {},
+        :matriarch => {},
+        :hero => {},
+        :hero_friends => []
+      },
+    :capulet => {
+      #your key/value pairs here!
+      }}
+        :patriarch => {},
+        :matriarch => {},
+        :heroine => {},
+        :heroine_friends => []
+      }
 }
- 
 
-  
+
+end
+
 end
